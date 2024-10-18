fabc539568bef1575fc3634047102dcdbfae88ea
Shraddhaw4 <134039137+Shraddhaw4@users.noreply.github.com>
2024-10-18 16:09:11 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+data "aws_iam_role" "glue-role"{
+  name = "glue-demo-role"
+}
