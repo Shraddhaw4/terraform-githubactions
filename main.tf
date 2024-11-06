96c4abd1d791abb70c7babf7a487a3c7720f002f
unknown <shraddha.a.waghukar@gmail.com>
2024-11-06 21:54:37 +0530

    Importing s3 buckets

--- main.tf
+++ main.tf
@@ -20,6 +20,10 @@ data "aws_iam_role" "glue-role"{
   name = "glue-demo-role"
 }
 
+data "aws_s3_bucket" "pyspark-data" {
+  bucket = "shraddha-pyspark-data"
+}
+
 resource "aws_iam_role" "test-role" {
   name = "Test-Role"
     assume_role_policy = jsonencode({
@@ -41,27 +45,27 @@ resource "aws_iam_policy_attachment" "pt" {
   roles = [ aws_iam_role.test-role.name ]
 }
 
-resource "aws_s3_bucket" "pyspark-data" {
-  bucket = "shraddha-pyspark-data"
-}
+# resource "aws_s3_bucket" "pyspark-data" {
+#   bucket = "shraddha-pyspark-data"
+# }
 
-resource "aws_s3_object" "folder_cust" {
-  bucket = aws_s3_bucket.pyspark-data.id
-  key    = "customers/customers.csv"
-  source = "pyspark-glue-tutorial-main/customers/customers.csv"
-}
+# resource "aws_s3_object" "folder_cust" {
+#   bucket = aws_s3_bucket.pyspark-data.id
+#   key    = "customers/customers.csv"
+#   source = "pyspark-glue-tutorial-main/customers/customers.csv"
+# }
 
-resource "aws_s3_object" "folder_emp" {
-  bucket = aws_s3_bucket.pyspark-data.id
-  key    = "employees/employees.csv"
-  source = "pyspark-glue-tutorial-main/employees/employees.csv"
-}
+# resource "aws_s3_object" "folder_emp" {
+#   bucket = aws_s3_bucket.pyspark-data.id
+#   key    = "employees/employees.csv"
+#   source = "pyspark-glue-tutorial-main/employees/employees.csv"
+# }
 
-resource "aws_s3_object" "folder_orders" {
-  bucket = aws_s3_bucket.pyspark-data.id
-  key    = "orders/orders.csv"
-  source = "pyspark-glue-tutorial-main/orders/orders.csv"
-}
+# resource "aws_s3_object" "folder_orders" {
+#   bucket = aws_s3_bucket.pyspark-data.id
+#   key    = "orders/orders.csv"
+#   source = "pyspark-glue-tutorial-main/orders/orders.csv"
+# }
 
 resource "aws_glue_catalog_database" "pyspark_db" {
   name        = "pyspark_tutorial_db"
@@ -74,7 +78,7 @@ resource "aws_glue_crawler" "cust-crawler" {
   role          = data.aws_iam_role.glue-role.arn
 
   s3_target {
-    path = "s3://${aws_s3_bucket.pyspark-data.bucket}/customers/"
+    path = "s3://${data.aws_s3_bucket.pyspark-data.bucket}/customers/"
   }
 }
 
@@ -84,6 +88,6 @@ resource "aws_glue_crawler" "order-crawler" {
   role          = data.aws_iam_role.glue-role.arn
 
   s3_target {
-    path = "s3://${aws_s3_bucket.pyspark-data.bucket}/orders/"
+    path = "s3://${data.aws_s3_bucket.pyspark-data.bucket}/orders/"
   }
 }
